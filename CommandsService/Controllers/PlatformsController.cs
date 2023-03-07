8406a4c90b3717c052428aa0243736a910a8585c
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-07 19:48:36 +0300

    create platforms test post connection

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+                
+        }
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine( "--> InBound Post # Command Services ! " );
+            return Ok("InBound test of from Platforms Controler ! ");
+        }
+    }
+}
